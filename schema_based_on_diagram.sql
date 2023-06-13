636e7467b0009a9c777c32a2904c63960a745d9b
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-06-13 14:02:56 -0600

    Add first 3 tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,23 @@
+CREATE TABLE patients (
+  id INT,
+  name VARCHAR(100),
+  date_of_birth DATE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories (
+  id INT,
+  admitted_at TIMESTAMP,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR(50),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE invoices (
+  id INT,
+  total_amount DECIMAL(6,2),
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id),
+  PRIMARY KEY(id)
+);
